92ef203372d74b8aeddf5b76d41fb261e786fad6
Bardala <102490981+Bardala@users.noreply.github.com>
2022-12-29 00:56:03 +0200

    getWorkouts & getWorkout

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,19 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
+const {
+  createWorkout,
+  getAllWorkouts,
+  getWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all workouts" });
-});
+router.get("/", getAllWorkouts);
 
 // GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single workout" });
-});
+router.get("/:id", getWorkout);
 
-router.post("/", async (req, res) => {
-  const { title, reps, load } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, reps, load });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 
 router.delete("/:id", (req, res) => {
   res.json({ mssg: "DELETE a workout" });
