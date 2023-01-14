dc80cf7cba5da8bb7fc94f019b22fb7d751e5cf2
Bardala <102490981+Bardala@users.noreply.github.com>
2023-01-14 19:14:02 +0200

    Logging users in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,16 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 // signup user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -38,4 +38,18 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) throw Error("All fields must be field");
+
+  const user = await this.findOne({ email });
+
+  if (!user) throw Error("Incorrect Email");
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) throw Error("Incorrect Password");
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
