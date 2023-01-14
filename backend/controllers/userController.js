6b6407941085d98e4109550db1b3fe16aa202dfd
Bardala <102490981+Bardala@users.noreply.github.com>
2023-01-14 15:40:53 +0200

    signing token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -11,8 +16,9 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
+    const token = createToken(user._id);
 
-    res.status(200).json({ email, user });
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
