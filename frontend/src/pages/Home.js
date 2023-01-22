28cbab78b6f393b121588b6ab47ae8ad99f20991
Bardala <102490981+Bardala@users.noreply.github.com>
2023-01-22 23:11:40 +0200

    Authintication Context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,10 +10,10 @@ const Home = () => {
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("/api/workouts");
-      const json = await response.json();
+      const data = await response.json();
 
       if (response.ok) {
-        dispatch({ type: "SET_WORKOUTS", payload: json });
+        dispatch({ type: "SET_WORKOUTS", payload: data });
       }
     };
 
